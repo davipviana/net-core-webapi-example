7da008169cdcfc0235ba46b573eb06c684440498
Davi Viana <davipviana@gmail.com>
2019-08-19 15:28:51 -0300

    Dependency injection

--- src/WebApi.Example/Startup.cs
+++ src/WebApi.Example/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebApi.Example.DependencyInjection;
 
 namespace WebApi.Example
 {
@@ -26,6 +27,8 @@ namespace WebApi.Example
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            DependencyInjector.Configure(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
