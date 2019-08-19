e8db1f043addf6509639a07552184c6f9305750b
Davi Viana <davipviana@gmail.com>
2019-08-19 16:40:33 -0300

    Persons controller

---
+++ src/WebApi.Example/Controllers/PersonsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Example.Domain.Entity;
+using WebApi.Example.Interface.Business;
+
+namespace WebApi.Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        #region Properties
+        private IPersonBusiness _personBusiness;
+        #endregion
+
+        #region Constructors
+        public PersonsController(IPersonBusiness personBusiness)
+        {
+            _personBusiness = personBusiness;
+        }
+        #endregion
+
+        // GET api/persons
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personBusiness.ListAll());
+        }
+
+        // GET api/persons/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok(_personBusiness.GetById(id));
+        }
+
+        // POST api/persons
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            _personBusiness.Create(person);
+
+            return Ok(person);
+        }
+
+        // PUT api/persons/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Person person)
+        {
+            _personBusiness.Update(id, person);
+
+            return Ok(person);
+        }
+
+        // DELETE api/persons/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _personBusiness.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
