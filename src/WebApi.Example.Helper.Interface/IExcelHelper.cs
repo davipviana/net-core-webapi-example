aaac81c3446a7d246e9254f1c1e86d2d5884931b
Davi Viana <davipviana@gmail.com>
2019-09-16 20:38:09 -0300

    Excel file generation helper

---
+++ src/WebApi.Example.Helper.Interface/IExcelHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WebApi.Example.Helper.Interface
+{
+    public interface IExcelHelper<T>
+    {
+        byte[] GenerateFile(IEnumerable<T> rowObjects, byte[] template, int rowOffset = 0);
+    }
+}
---
+++ src/WebApi.Example.Helper/ExcelHelper.cs
@@ -0,0 +1,38 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WebApi.Example.Helper.Interface;
+
+namespace WebApi.Example.Helper
+{
+    public class ExcelHelper<T> : IExcelHelper<T>
+    {
+        #region IExcelHelper methods
+        public byte[] GenerateFile(IEnumerable<T> rowObjects, byte[] template, int rowOffset = 0)
+        {
+            var memStream = new MemoryStream();
+            memStream.Write(template, 0, template.Length);
+            memStream.Seek(0, SeekOrigin.Begin);
+            
+            using (var package = new ExcelPackage(memStream))
+            {
+                var worksheet = package.Workbook.Worksheets.First();
+
+                for (var i = 0; i < rowObjects.Count(); i++)
+                {
+                    var rowObject = rowObjects.ElementAt(i);
+
+                    var properties = rowObject.GetType().GetProperties();
+                    for (var j = 0; j < properties.Count(); j++)
+                    {
+                        worksheet.Cells[i + 1 + rowOffset, j + 1].Value = properties[j].GetValue(rowObject).ToString();
+                    }
+                }
+                memStream.Dispose();
+                return package.GetAsByteArray();
+            }
+        }
+        #endregion
+    }
+}
